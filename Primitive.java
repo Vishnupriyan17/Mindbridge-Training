30483531de10d79d351955845f4fd5458d34c26f
Vishnupriyan17 <vishnukodees@gmail.com>
2024-12-26 23:10:52 +0530

    Day 1 Practice

--- Primitive.java
+++ Primitive.java
@@ -4,13 +4,13 @@ public class Primitive {
         boolean Graduate = true;
         char alphabet = 'a';
         byte number = 001;
-        int DOB = 17092003;
+        int dateOfBirth = 17092003;
         short age = 21;
-        long regNum = 2121222;
+        long registerNumber = 2121222;
         float mark = 88.5f;
         double intrest = 5467.789;
         System.out.print(Graduate+"\n"+alphabet+"\n"+number+"\n"
-                         +DOB+"\n"+age+"\n"+regNum+"\n"+mark+"\n"+intrest+"\n");
+                         +dateOfBirth+"\n"+age+"\n"+registerNumber+"\n"+mark+"\n"+intrest+"\n");
         int amount = (int)intrest;
         System.out.println(amount);
     }
