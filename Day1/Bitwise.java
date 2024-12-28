4fcb96eb8936429144f019d538b9da9229eeb3d8
Vishnupriyan M K <152880317+Vishnupriyan17@users.noreply.github.com>
2024-12-28 12:47:59 +0530

    Delete Day1 directory

--- Day1/Bitwise.java
+++
@@ -1,7 +0,0 @@
-public class Bitwise {
-    public static void main(String[] args) {
-        int number1 = 2;
-        System.out.println("This is Left Shift: " + (number1 << 3));
-        System.out.println("This is Right Shift: " + (number1 >> 2));
-    }
-}
--- Day1/TypeConversion.java
+++
@@ -1,9 +0,0 @@
-public class TypeConversion {
-    public static void main(String[] args)
-    {
-        float mark = 88.5f;
-
-        int marks = (int) mark;
-        System.out.println("This is float value: "+mark+"\n"+"This is Integer value: "+marks);
-    }
-}
