5509a71e022c234ce3b122149d18e044de6f0447
Vishnupriyan17 <vishnukodees@gmail.com>
2025-01-06 16:00:30 +0530

    Day 8 Practice

--- Day8/Rotate.java
+++ Day8/Rotate.java
@@ -1,4 +1,20 @@
 package Day8;
 
+import java.util.Arrays;
+
 public class Rotate {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7};
+        int k =3;
+        for(int i =0;i<k;i++)
+        {
+            int temp = arr[arr.length-1];
+            for(int j = arr.length-1;j>0;j--)
+            {
+                arr[j] = arr[j-1];
+            }
+            arr[0] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
