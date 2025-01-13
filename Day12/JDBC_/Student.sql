db9fc82287db6f59e34ae2670278cc48c0f6c634
Vishnupriyan M K <152880317+Vishnupriyan17@users.noreply.github.com>
2025-01-13 09:51:41 +0530

    Add files via upload

---
+++ Day12/JDBC_/Student.sql
@@ -0,0 +1,14 @@
+show databases;
+create database Student;
+use Student;
+
+create table Student_Details(
+	id int,
+	name varchar(10),
+    mark int
+);
+
+select * from Student_details;
+truncate table Student_details;
+
+
