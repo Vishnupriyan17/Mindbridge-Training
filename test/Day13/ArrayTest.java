184a72ff5bed3c847a526192c5641004fdc55c4e
Vishnupriyan17 <vishnukodees@gmail.com>
2025-01-20 20:52:11 +0530

    Training Practices

--- test/Day13/ArrayTest.java
+++ test/Day13/ArrayTest.java
@@ -11,32 +11,32 @@ public class ArrayTest {
     {
         int[] input = {2,3,4,1,5,6};
         String expected = "[1, 2, 3, 4, 5, 6]";
-        assertEquals(expected, Day13.Array1.performSorting(input));
+        assertEquals(expected, Day13.Array.performSorting(input));
     }
     @Test
     public void duplicateArray()
     {
         int[] input = {2,3,4,1,5,5};
         String expected = "[1, 2, 3, 4, 5, 5]";
-        assertEquals(expected, Day13.Array1.performSorting(input));
+        assertEquals(expected, Day13.Array.performSorting(input));
     }
     @Test
     public void sortedArray()
     {
-        assertEquals("[1, 2, 3, 4, 5, 6]", Day13.Array1.performSorting(new int[]{1,2,3,4,5,6}));
+        assertEquals("[1, 2, 3, 4, 5, 6]", Day13.Array.performSorting(new int[]{1,2,3,4,5,6}));
     }
     @Test
     public void emptyArray()
     {
         int[] input = {};
         String expected = "[]";
-        assertEquals(expected, Day13.Array1.performSorting(input));
+        assertEquals(expected, Day13.Array.performSorting(input));
     }
     @Test
     public void negativeArray()
     {
         int[] input = {-2,-3,-4,-1,-5,-6};
         String expected = "[-6, -5, -4, -3, -2, -1]";
-        assertEquals(expected, Day13.Array1.performSorting(input));
+        assertEquals(expected, Day13.Array.performSorting(input));
     }
 }
